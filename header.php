e8136d8642ed27a87109dd5d3dd1f6fd89060967
Gretchen Thomas <gsthomas612@gmail.com>
2025-12-26 17:07:02 -0700

    Update header navbar transparency and rebuild deps

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 </head>
 
 <?php
-$navbar_scheme   = get_theme_mod('navbar_scheme', 'navbar-light bg-transparent'); // Get custom meta-value.
+$navbar_scheme   = get_theme_mod('navbar_scheme'); // Get custom meta-value.
 $navbar_position = get_theme_mod('navbar_position', 'static'); // Get custom meta-value.
 
 $search_enabled  = get_theme_mod('search_enabled', '1'); // Get custom meta-value.
